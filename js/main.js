60128a0d14c90fc6122cde5c2daf52b2147bceab
Durran Salah <dosalah23@gmail.comm>
2022-12-14 20:50:33 -0800

    highlight added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,73 @@
+//let actors = document.querySelectorAll(".hamlet");
+ 
+//console.log(actors);
+ 
+//for (let i = 0; i < actors.length; i++) {
+ //actors[i].style.backgroundColor = "orange";
+
+
+    //function highlight(actor){
+    //el.style.backgroundColor='blue';
+    
+
+  
+
+    
+
+
+    //function myAlert(){
+    //alert("I'm clicked!");
+
+
+    function highlightActor(actor){
+
+        let spans = document.querySelectorAll("#play span");
+        for(const mySpan of spans){
+        
+            if (actor == mySpan.dataset.actor)
+                {//white, then turn blue
+                mySpan.style.backgroundColor='yellow';
+        
+                }else{//turn white
+                mySpan.style.backgroundColor='white';
+        
+                }
+        }
+    }
+
+   
+
+
+
+
+let spans = document.querySelectorAll("#play span");
+
+ console.log(spans);
+
+ for(const mySpan of spans){
+
+    //mySpan.addEventListener("click",myAlert);
+    //
+    mySpan.addEventListener("click", function(ev) {
+        highlightActor(mySpan.dataset.actor);
+        //alert(mySpan.dataset.actor);
+    
+
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 //https://tinyurl.com/dynamic-html-checker
 document.getElementById("html-checker").setAttribute("href","https://validator.w3.org/nu/?doc=" + location.href);
     
